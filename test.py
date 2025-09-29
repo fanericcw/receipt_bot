dc35e79768259547627c53f4ca518e9639e21126
fanericcw <fan.ericcw@gmail.com>
2025-09-29 03:05:53 -0400

    Add .env to gitignore and version files

--- test.py
+++
@@ -1,5 +0,0 @@
-import pytesseract
-from PIL import Image
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-with Image.open("receipt23.jpg") as ext:
-    print(pytesseract.image_to_string(ext))
